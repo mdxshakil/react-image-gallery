a8932d69b8a2211c6604617e34c793ba1040f145
mdxshakil <mdxshakil1767@gmail.com>
2023-11-01 16:02:56 +0600

    Project initialization

---
+++ src/App.tsx
@@ -0,0 +1,102 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [selectedImage, setSeletedImage] = useState<Array<number>>([]);
+  const images = [
+    {
+      id: 1,
+      link: "https://images.unsplash.com/photo-1420593248178-d88870618ca0?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bmF0dXJhbHxlbnwwfHwwfHx8MA%3D%3D",
+    },
+
+    {
+      id: 3,
+      link: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.34264412.1698796800&semt=ais",
+    },
+    {
+      id: 4,
+      link: "https://www.nationalforests.org/assets/header-images/_945x590_fit_center-center_80_none/DSC_9578.JPG",
+    },
+    {
+      id: 5,
+      link: "https://wallpapers.com/images/featured/nature-2ygv7ssy2k0lxlzu.jpg",
+    },
+    {
+      id: 2,
+      link: "https://i.pinimg.com/736x/6c/f5/82/6cf582f4019d7d75101a3a15d24e0eb2.jpg",
+    },
+    {
+      id: 6,
+      link: "https://t3.ftcdn.net/jpg/05/35/47/38/360_F_535473874_OWCa2ohzXXNZgqnlzF9QETsnbrSO9pFS.jpg",
+    },
+    {
+      id: 7,
+      link: "https://us.123rf.com/450wm/amjid555/amjid5552304/amjid555230400520/201835917-crystal-ball-in-green-forest-with-ferns-3d-rendering-generative-ai.jpg?ver=6",
+    },
+    {
+      id: 8,
+      link: "https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
+    },
+    {
+      id: 9,
+      link: "https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2023/06/nature-1686808887.jpg",
+    },
+    {
+      id: 10,
+      link: "https://images.unsplash.com/photo-1546587348-d12660c30c50?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bmF0dXJhbHxlbnwwfHwwfHx8MA%3D%3D",
+    },
+    {
+      id: 11,
+      link: "https://learn.corel.com/wp-content/uploads/2022/01/alberta-2297204_1280.jpg",
+    },
+  ];
+
+  const handleImageSelection = (id: number) => {
+    if (selectedImage.includes(id)) {
+      setSeletedImage(selectedImage.filter((imageId) => id != imageId));
+    } else {
+      setSeletedImage([...selectedImage, id]);
+    }
+  };
+
+  
+
+  return (
+    <main className="max-w-6xl mx-auto font-primary">
+      <h1 className="text-4xl font-extrabold tracking-tight text-center my-4">
+        Welcome to the image gallery
+      </h1>
+      {selectedImage.length > 0 && (
+        <div className="flex gap-3 my-2">
+          <p className="font-bold">
+            Total {selectedImage.length} images selected
+          </p>
+          <button
+            className="bg-red-400 px-2 text-white text-sm rounded-full"
+            onClick={() => setSeletedImage([])}
+          >
+            Clear
+          </button>
+        </div>
+      )}
+      <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-3 mt-6">
+        {images.map((image) => (
+          <div className="mb-3 w-full break-inside-avoid" key={image.id}>
+            <img
+              className={`w-full rounded-lg transition-all cursor-pointer px-2 md:px-0 ${
+                selectedImage.includes(image.id)
+                  ? " opacity-50 border-red-400"
+                  : "scale-100"
+              }`}
+              src={image.link}
+              alt="Image"
+              onClick={() => handleImageSelection(image.id)}
+            />
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
+
+export default App;
